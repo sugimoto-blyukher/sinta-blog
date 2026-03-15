24759e38404e67b2fa6869dc17cb31b070f1046a
sugimoto-blyukher <143075979+sugimoto-blyukher@users.noreply.github.com>
2026-03-15 16:03:59 +0900

    Update @sveltejs/adapter-cloudflare to version 7.2.8 and refactor svelte.config.js for improved adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,23 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
 	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.svx'],
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			config: undefined,
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			},
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
 
 export default config;
